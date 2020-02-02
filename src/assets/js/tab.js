6d6673c5d5a878b6614e49e5b2071c26a0b19774
maksymplish <plish.max@gmail.com>
2020-02-02 15:47:34 +0200

    first commit

---
+++ src/assets/js/tab.js
@@ -0,0 +1,5 @@
+$(".ba-card-item").not(":first").hide();
+$(".ba-tab").click(function () {
+	$(".ba-tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".ba-card-item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active");
